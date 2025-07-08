fa9eebb06d6f37a8e4b41cc70d5b70dfb2a2e8c0
metakael <timsty@gmail.com>
2025-07-08 17:34:17 +0800

    Add cong files and dependencies.

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// File: postcss.config.js
+// Purpose: Configures PostCSS, which is used by Tailwind CSS.
+// Location: Place this in the root of your project folder.
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// File: tailwind.config.js
+// Purpose: Configures Tailwind CSS.
+// Location: Place this in the root of your project folder.
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
